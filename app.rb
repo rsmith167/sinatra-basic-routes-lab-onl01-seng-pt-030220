50c4a89ca93ead2365a4cf1474998716174196eb
Rwanda S <rwanda.ace@gmail.com>
2020-06-23 16:36:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+get '/' name do
+  "My name is Rwanda"  
+end
+
+get '/' hometown do
+  "My hometown is Atlanta"
+end
+get '/' favorite_song do
+  "My favorite song is P"
+end
+  
 end
