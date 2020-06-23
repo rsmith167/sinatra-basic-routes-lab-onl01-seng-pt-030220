c137b1e04f02b23a73fe4fd59520ff66b755e65a
Rwanda S <rwanda.ace@gmail.com>
2020-06-23 16:38:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
-
+#require 'sinatra'
 class App < Sinatra::Base
+  
+get '/name'  do
+  "My name is Rwanda"  
+end
+
+get '/hometown'  do
+  "My hometown is Atlanta"
+end
+get '/favorite-song'  do
+  "My favorite song is P"
+end
+  
 end
